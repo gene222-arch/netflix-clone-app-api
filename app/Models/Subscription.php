677f71119b1f35b43704b3cf575358e37b29620c
gene222-arch <genephillip222@gmail.com>
2021-09-28 10:30:33 +0800

    feat: added is first subscription field to table

--- app/Models/Subscription.php
+++ app/Models/Subscription.php
@@ -14,6 +14,7 @@ class Subscription extends Model
     protected $fillable = [
         'user_id',
         'type',
+        'is_first_subscription',
         'subscribed_at',
         'expired_at',
         'cancelled_at'
