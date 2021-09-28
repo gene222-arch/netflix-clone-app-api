9de2492df8cf29f99f2678e7139adcb1fa6326d0
gene222-arch <genephillip222@gmail.com>
2021-09-28 10:42:21 +0800

    feat: add auto insertion while creating

--- app/Models/Subscription.php
+++ app/Models/Subscription.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -19,4 +20,28 @@ class Subscription extends Model
         'expired_at',
         'cancelled_at'
     ];
+
+    protected static function booted()
+    {
+        parent::boot();
+
+        static::creating(function ($subscription) 
+        {
+            $subscription->user_id = auth('api')->id();
+            $subscription->subscribed_at = Carbon::now();
+            $subscription->cancelled_at = null;
+
+            if (! auth('api')->user()->subscriptions->count()) 
+            {
+                $subscription->is_first_subscription = true;
+
+                $subscription->expired_at = $subscription->expired_at->addMonth();
+            }
+        });
+    }
+
+    public function subscribe(string $type)
+    {
+        return $this->query()->create([ 'type' => $type ]);
+    }
 }
