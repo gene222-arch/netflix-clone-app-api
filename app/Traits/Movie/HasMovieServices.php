7ea2bda8cbbeafe6fdde03a9c1d705d13d0b1051
gene222-arch <genephillip222@gmail.com>
2021-10-04 20:00:17 +0800

    fix: recently added movies order

--- app/Traits/Movie/HasMovieServices.php
+++ app/Traits/Movie/HasMovieServices.php
@@ -75,7 +75,10 @@ trait HasMovieServices
             {
                 $recentlyAddedMovies = Movie::query();
                 $recentlyAddedMovies->when($isForKids, fn($q) => $q->where('age_restriction', '<=', 12));
-                $recentlyAddedMovies = $recentlyAddedMovies->where('created_at', '<=', Carbon::now()->addDays(14))->get();
+                $recentlyAddedMovies = $recentlyAddedMovies->where('created_at', '<=', Carbon::now()->addDays(14))
+                    ->orderBy('created_at', 'desc')
+                    ->orderBy('updated_at', 'desc')
+                    ->get();
                 
 
                 $trendingNow = Movie::selectRaw('
