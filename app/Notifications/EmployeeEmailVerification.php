d7acd92f0be8db6804d4086e535edef3db6b6192
gene222-arch <genephillip222@gmail.com>
2021-10-12 21:53:31 +0800

    feat: create EmployeeEmailVerification

---
+++ app/Notifications/EmployeeEmailVerification.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+use Illuminate\Support\HtmlString;
+
+class EmployeeEmailVerification extends Notification
+{
+    use Queueable;
+
+    public string $password = '';
+
+    /**
+     * Create a new notification instance.
+     *
+     * @return void
+     */
+    public function __construct(string $password)
+    {
+        $this->password = $password;
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function via($notifiable)
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return \Illuminate\Notifications\Messages\MailMessage
+     */
+    public function toMail($notifiable)
+    {
+        $hashedEmail = sha1($notifiable->email);
+        $urlVerificationPath = "http://localhost:3000/employee/email-verification?token=$hashedEmail";
+
+        return (new MailMessage)
+                    ->subject('Email Verification')
+                    ->greeting('Hello Sir/Maam, ' . $notifiable->first_name)
+                    ->line('')
+                    ->line('The following will be your permanent ' . env('APP_NAME') . ' account.')
+                    ->line('Upon opening your account please do change your password.')
+                    ->line(new HtmlString('<strong>Email: </strong>' . $notifiable->email))
+                    ->line(new HtmlString('<strong>Password: </strong>' . $this->password))
+                    ->line('')
+                    ->action('Notification Action', url($urlVerificationPath));
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function toArray($notifiable)
+    {
+        return [
+            //
+        ];
+    }
+}
