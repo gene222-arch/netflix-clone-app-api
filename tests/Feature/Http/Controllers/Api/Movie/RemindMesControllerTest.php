4ae0f1773407e8e9aaca7760a7b535748c86f3d0
gene222-arch <genephillip222@gmail.com>
2021-10-27 08:01:33 +0800

    test: add user_can_mark_as_read

--- tests/Feature/Http/Controllers/Api/Movie/RemindMesControllerTest.php
+++ tests/Feature/Http/Controllers/Api/Movie/RemindMesControllerTest.php
@@ -24,4 +24,23 @@ class RemindMesControllerTest extends TestCase
 
         $this->assertResponse($response);
     }
+
+    /** test */
+    public function user_can_mark_as_read()
+    {
+        $data = [
+            'user_profile_id' => 4,
+            'coming_soon_movie_id' => 6
+        ];
+
+        $response = $this->put(
+            '/api/remind-mes/mark-as-read',
+            $data,
+            $this->apiHeader()
+        );
+
+        $this->assertResponse($response);
+    }
+
+
 }
