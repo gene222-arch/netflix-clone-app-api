e376427bdabcbecaf3be4b9a6a6c3f61a3dc5a87
gene222-arch <genephillip222@gmail.com>
2021-10-31 08:16:11 +0800

    feat: customize mailable template

---
+++ resources/views/vendor/mail/html/header.blade.php
@@ -0,0 +1,10 @@
+<tr>
+    <td class="header">
+        <a href="{{ $url }}" style="display: inline-block;">
+            <img 
+                src="https://laravel-flicklify-files.s3.ap-southeast-1.amazonaws.com/app/app-logo.png?response-content-disposition=inline&X-Amz-Security-Token=IQoJb3JpZ2luX2VjEEgaDmFwLXNvdXRoZWFzdC0xIkgwRgIhAJQ0KpQLDNjyuEwofiSWI07UHTdoMrCwCdV52PctYxIRAiEAoc20MuEFZJC%2B5M7faDy9Q1Bfnf3JYIWnf8rJ2qW4EPMq%2FwII4f%2F%2F%2F%2F%2F%2F%2F%2F%2F%2FARAAGgwxNDAwNjg1NTA5NTUiDG4%2FtTStOWvBQ6%2FsMCrTAsnjNn1sW%2Fc9lS3nDcElKWbp8%2Bgmhldc6P1D%2Ba6imAOkxPUZ95W%2BCmC2by1Wq%2Bu23TvVhb7k8G5zwoRAEG2cHjc0mI2h0bW%2B9qBAKMImDSRslya6S%2FS3E6dKowW%2B9csE%2BnkeBVcfYPY1t2NixM6DKmbOxQsIyd1o9QlxpcjDJz8GL32M0RtJ0fbuPlvjdDafLJiWQgaDjff020Fmpo0VYTCq83be2XbulK2h85EVPHK%2B%2FlQQgnUuhpbayeSm6gs4vqi63Ncr6LjlPfpwCDfqHoR%2FimqqaCLT5AY0tm7NJRVFDckLeuz34fCxNsrXpM9ViSvj4saxaPsi8xFQ9uF1iIKpkqNwmd7yMirj%2BTbOJOnf%2F%2Ff3u7KbGJrlXL7OzE%2BXW%2Fwq%2FGaVAl9Y3wa9SKZiClXft6NjoLLODW6poE2RxBFyaMrSagB2ksPJEZpp267bzriXHDCHu%2FeLBjqyAuv%2Fu1A1ANJum1ItUnGMASPxCvh1A%2F0lV6MNKuKtSBaGhc%2F2QEYG0kH3d2zE0vt6fvyxRA9jS%2FHfMj%2Bx7OB3nBm8x1DOliqrCzJbwtRAUllImevInfGQT0IISEWQ2dEV5SY%2BTgMV8vJDzBOkrHfnEHPOSd9FBPGlmpsvlHZW9TmRcQs3qXf1yF%2F%2B%2FabIy50fbdt3QZcLBqV0CwA70hkh8wviLw%2FVnZT%2F1FaIFedc7BqMEoahHXmIrpdjMoRr5Imlr0gvf4xr3QDaCNVF7e86PJBxnMn%2BSlstJF1OK6nandsfOgrkOQ1ggHC1d5Y5deMaNLiJW%2FRz17SAw2UuAxJpAnfx0QOLrhPpTevOwp7F%2BxTtIUHEahTY9ZYNE8JhiYVcqWK7jxvXT%2B5OKjcG%2FnukIb3JyQ%3D%3D&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Date=20211031T000625Z&X-Amz-SignedHeaders=host&X-Amz-Expires=300&X-Amz-Credential=ASIASBHF4PEV2SF6JAWV%2F20211031%2Fap-southeast-1%2Fs3%2Faws4_request&X-Amz-Signature=06718346d72458973f43a9a500c6b222c5dd7626baa6a1b6acac41da3b28c954"
+                class="logo"
+            >
+        </a>
+    </td>
+</tr>
