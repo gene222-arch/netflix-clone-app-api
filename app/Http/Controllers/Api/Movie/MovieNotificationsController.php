4e468972fd77dc41a16e08e0b6469526efa16ad1
gene222-arch <genephillip222@gmail.com>
2021-11-19 16:43:12 +0800

    feat: add filter is for kids movie type

--- app/Http/Controllers/Api/Movie/MovieNotificationsController.php
+++ app/Http/Controllers/Api/Movie/MovieNotificationsController.php
@@ -15,13 +15,20 @@ class MovieNotificationsController extends Controller
      */
     public function index()
     {
-        $notifications = MovieNotification::with(
-                'movie:id,title,wallpaper_path', 
-                'releasedDetails:id,movie_id,coming_soon_movie_id'
-            )
+        $isForKids = (bool) request()->input('isForKids');
+
+        $operation = $isForKids ? '<=' : '>=';
+        $ageRestriction = $isForKids ? 12 : 0;
+
+        $notifications = MovieNotification::with([
+            'movie' => fn($q) => $q->where('age_restriction', $operation, $ageRestriction), 
+            'releasedDetails:id,movie_id,coming_soon_movie_id'
+        ])
             ->where('created_at', '<=', Carbon::now()->addMonth())
             ->orderByDesc('created_at')
             ->get();
+
+        $notifications = $notifications->filter(fn ($notification) => $notification->movie);
 
         return !$notifications->count() ? $this->noContent(): $this->success($notifications);
     }
