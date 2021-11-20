ecf9db59af7489bf08e34e9b734b79364c5efeea
gene222-arch <genephillip222@gmail.com>
2021-11-20 16:53:09 +0800

    feat: implement should queue

--- app/Notifications/EmployeeEmailVerification.php
+++ app/Notifications/EmployeeEmailVerification.php
@@ -8,7 +8,7 @@ use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
 use Illuminate\Support\HtmlString;
 
-class EmployeeEmailVerification extends Notification
+class EmployeeEmailVerification extends Notification implements ShouldQueue
 {
     use Queueable;
 
