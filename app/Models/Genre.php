771b2af9dcc6e0acacadfa5b6e59d056d593803a
gene222-arch <genephillip222@gmail.com>
2021-12-05 18:19:08 +0800

    fix: Integrity constraint violation: 1452

--- app/Models/Genre.php
+++ app/Models/Genre.php
@@ -2,11 +2,11 @@
 
 namespace App\Models;
 
-use App\Models\Movie;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
-class Genre extends Movie
+class Genre extends Model
 {
     use HasFactory;
 
