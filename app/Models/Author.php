93ad908a758c2d59f6caf39324bbcdd4377cc553
gene222-arch <genephillip222@gmail.com>
2021-12-10 16:59:29 +0800

    feat: add deleted_at field to authors table

--- app/Models/Author.php
+++ app/Models/Author.php
@@ -5,10 +5,11 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Author extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
 
     protected $fillable = [
         'avatar_path',
@@ -27,7 +28,8 @@ class Author extends Model
 
     protected $hidden = [
         'created_at',
-        'updated_at'
+        'updated_at',
+        'deleted_at'
     ];
 
     /**
