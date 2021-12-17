6d43bbb73b4a59b4983c5e9080d3e11877300b91
gene222-arch <genephillip222@gmail.com>
2021-12-17 10:35:49 +0800

    feat: disable laravel-websockets dashboard public access

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -26,10 +26,6 @@ class AuthServiceProvider extends ServiceProvider
     {
         $this->registerPolicies();
 
-        Gate::define('viewWebSocketsDashboard', function ($user = null) {
-            return true;
-        });
-
         if (! $this->app->routesAreCached()) {
             Passport::routes();
         }
