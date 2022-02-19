80fd117d667d878fef3f981d3ab175121f834e7d
gene222-arch <genephillip222@gmail.com>
2022-02-19 23:07:28 +0800

    feat: check if user has recently watched movies

--- app/Http/Controllers/Api/User/UserProfilesController.php
+++ app/Http/Controllers/Api/User/UserProfilesController.php
@@ -56,8 +56,11 @@ class UserProfilesController extends Controller
             ])
             ->find($id);
 
-        $profileDetails->recently_watched_movies = $profileDetails
-            ?->recentlyWatchedMovies
+        $profileDetails->recently_watched_movies = [];
+
+        if ($profileDetails->recentlyWatchedMovies)
+        {
+            $profileDetails->recentlyWatchedMovies
             ->map(function ($recentlyWatchedMovie) 
             {
                 $movie = $recentlyWatchedMovie->movie;
@@ -67,7 +70,8 @@ class UserProfilesController extends Controller
                 return $movie;
             });
 
-        unset($profileDetails->recentlyWatchedMovies);
+            unset($profileDetails->recentlyWatchedMovies);
+        }
 
         return $this->success($profileDetails);
     }
